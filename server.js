266c673dd533debd9eac4f4001457b9d9fd9e263
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-12-03 12:57:01 -0500

    reorganized server file; added passport and session npm requirements

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
+//Requiring necessary npm packages
 const express = require("express");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
+
+//Getting passport as configured
+const passport = require("./config/passport");
+
+//Requiring dev npm packages
 const morgan = require("morgan");
-const db = require("./models/index");
+
+//Port setup and requiring db model for syncing
 const PORT = process.env.PORT || 4650;
+const db = require("./models/index");
+
+//Setting up express app and its middleware
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 // View Engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan("dev"));
+// Use sessions to keep track of user login status
+const sess = {
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {}
+}
+// Use secure cookies in production (because the site is https-enabled) & allow for testing in dev
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1)
+    sess.cookie.secure = true
+}
+app.use(session(sess));
+
+app.use(passport.initialize());
+app.use(passport.session())
 
 // Routes
 app.use(require("./controllers/staticController"));
 
 //Synchronize my schema
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: process.env.NODE_ENV !== "production" }).then(() => {
     app.listen(PORT, () => {
         console.log("Server listening on: http://localhost:" + PORT);
     });
