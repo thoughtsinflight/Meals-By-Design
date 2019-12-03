8493eb8305d8a1ddd5d05a13e175ca3450d5ef91
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-12-03 14:00:12 -0500

    recovered my code

---
+++ config/passport.js
@@ -0,0 +1,51 @@
+// Dependencies required
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("../models");
+
+passport.use(new LocalStrategy(
+    //Email is generally easier than creating a username so we're creating that in the place of username
+    {usernameField: "email"},
+    function(email, password, done) {
+    // User tries to sign in, passport will look for a matching user
+        db.User.findOne({ 
+            where: {
+                email: email
+            } 
+        }).then( dbUser => {
+            // If the email is incorrect
+            if(!dbUser){
+                return done(null, false, {
+                    message: "There's no user with that email."
+                });
+            // If the email is right but the password is wrong
+            }else if(!dbUser.goodPass(password)) {
+                return done(null, false, {
+                    message: "That password is invalid."
+                });
+            //If everything is right, return the user data
+            }else {
+                return done(null, dbUser)
+            }
+        });
+    }
+));
+
+// Restore/remember authentication state across the HTTP requests during the user's session
+// Serializing/deserializing the user data needs to happen
+passport.serializeUser( (user, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser( (id, cb) => {
+    db.user.findById(id, (err, user) => {
+        if (err) {
+            return cb(err)
+        }else{
+            cb(null, user)
+        }
+    });
+});
+
+//Exporting the passport configuration for use in the server
+module.exports = passport;
