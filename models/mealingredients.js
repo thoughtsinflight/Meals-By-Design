51466775addc7821fab8faa30ad63fb3e74b6c87
Christie Burt <cmburt@yahoo.com>
2019-12-03 13:50:56 -0500

    minor fixes -- capitalization sequelize-style, etc.

--- models/mealingredients.js
+++ models/mealingredients.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const MealIngredients = sequelize.define('MealIngredients', {
-    mealId: DataTypes.INTEGER,
-    ingredientId: DataTypes.INTEGER
+    MealId: DataTypes.INTEGER,
+    IngredientId: DataTypes.INTEGER
   }, {});
   MealIngredients.associate = function(models) {
     // associations can be defined here
