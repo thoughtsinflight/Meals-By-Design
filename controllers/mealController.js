f32e8571a6e3196589efb67c5e0808b30a716360
Christie Burt <cmburt@yahoo.com>
2019-12-05 09:40:56 -0500

    somehow deleted post from meal controller. added back.

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -7,6 +7,37 @@ const Ingredient = db.sequelize.import(path.resolve(__dirname, "../models/ingred
 const Day = db.sequelize.import(path.resolve(__dirname, "../models/day.js"));
 const User = db.sequelize.import(path.resolve(__dirname, "../models/user.js"));
 
+
+
+//create
+router.post("/", async (req, res) => {
+    //req.body.ingredients should be an array of objects of the format:
+    // [{name: "flour"}, {name: "butter"}]
+    //need to get user id from session here.
+    const userId = 1;
+    const { name: mealName, ingredients, dayId } = req.body;
+    //Find Day
+    const day = await Day.findByPk(dayId);
+    //Find User
+    const user = await User.findByPk(userId);
+    //Find or Create meal
+    const [meal] = await Meal.findOrCreate({
+        where: { name: mealName }
+    });
+
+    await meal.setDay(day);
+    await meal.setUser(user);
+
+    ingredients.forEach(async (ingredient) => {
+        const [focIngredient] = await Ingredient.findOrCreate({ where: ingredient });
+        await meal.addIngredient(focIngredient);
+    });
+
+    const currentIngredients = await meal.getIngredients();
+    res.send({ day, meal, currentIngredients });
+});
+
+
 //get - read
 
 //get all meals sans ingredients
